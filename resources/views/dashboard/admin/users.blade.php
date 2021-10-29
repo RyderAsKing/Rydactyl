f15f394a45d2c3989122ea38370a9175aef88c94
RyderAsking <clasherlodt@gmail.com>
2021-10-29 19:57:21 +0530

    Made users table look a bit good

--- resources/views/dashboard/admin/users.blade.php
+++ resources/views/dashboard/admin/users.blade.php
@@ -13,14 +13,17 @@
                         <h4 class="card-title">Users</h4>
                         </p>
                         <div class="table-responsive">
-                            <table class="table table-bordered" id="users-table">
+                            <table class="table table-bordered" id="users-table" style="margin-bottom: 15px">
                                 <thead>
                                     <tr>
-                                        <th> # </th>
-                                        <th> Title </th>
-                                        <th> Message </th>
-                                        <th> Created at </th>
-                                        <th> Remove </th>
+                                        <th> Username</th>
+                                        <th> Coin balance</th>
+                                        <th> RAM Balance</th>
+                                        <th> CPU Balance</th>
+                                        <th> DISK Balance</th>
+                                        <th> Slots Balance</th>
+                                        <th> RAM Used </th>
+                                        <th> Actions </th>
                                     </tr>
                                 </thead>
                                 <tbody>
@@ -28,14 +31,23 @@
                                         <td colspan="5" align="center">No users</td>
                                         </tr>
                                         @else
-                                        @foreach ($users as $notification)
+                                        @foreach ($users as $user)
                                         <tr>
-                                            <td> {{ $notification->discord_id }} </td>
-                                            <td> {{ $notification->username }} </td>
-                                            <td> {{ $notification->coin_balance }} </td>
-                                            <td> {{ $notification->created_at->diffForHumans() }} </td>
-                                            <td> <a href=""><button class="btn btn-danger"><i
-                                                            class="mdi mdi-delete"></i></button></a> </td>
+                                            <td @if($user->type == 1) style="color: #00D25B" @endif> {{ $user->username
+                                                }}
+                                                ({{ $user->discord_id }}) </td>
+                                            <td> {{ $user->coin_balance }} </td>
+                                            <td> {{ $user->ram_balance }} </td>
+                                            <td> {{ $user->disk_balance }} </td>
+                                            <td> {{ $user->cpu_balance }} </td>
+                                            <td> {{ $user->slot_balance }} </td>
+                                            <td> {{ $user->total_ram_balance - $user->ram_balance }}
+                                            </td>
+                                            <td> @if($user->type != 1) <a
+                                                    href="{{ route('dashboard.users.delete', $user->id) }}"><button
+                                                        class="btn btn-danger"><i
+                                                            class="mdi mdi-delete"></i></button></a> @else <span
+                                                    class="badge badge-success">Admin</span> @endif </td>
                                         </tr>
                                         @endforeach
                                         @endif
