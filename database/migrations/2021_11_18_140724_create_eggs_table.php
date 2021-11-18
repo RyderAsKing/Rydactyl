3ac49b4c9742306927b0002b1693b05b3c5335ca
RyderAsking <clasherlodt@gmail.com>
2021-11-18 19:40:45 +0530

    Nests and eggs

---
+++ database/migrations/2021_11_18_140724_create_eggs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEggsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('eggs', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->string('uuid');
+            $table->integer('nest_id');
+            $table->integer('egg_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('eggs');
+    }
+}
