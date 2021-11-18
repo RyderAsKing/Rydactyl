3c59cb771e73e73cf1a53dd88414c4ed018933ec
RyderAsking <clasherlodt@gmail.com>
2021-11-18 20:20:06 +0530

    Add nest template

--- resources/views/dashboard/nests/add.blade.php
+++ resources/views/dashboard/nests/add.blade.php
@@ -8,7 +8,7 @@
             <div class="col-12 mb-3">
                 <div class="card">
                     <div class="card-body">
-                        <h3>Make sure each node are in their seperate location or this system will not work as expected.
+                        <h3>Make sure each nest are in their seperate location or this system will not work as expected.
                         </h3>
                     </div>
                 </div>
@@ -16,55 +16,24 @@
             <div class="col-md-12 grid-margin stretch-card">
                 <div class="card">
                     <div class="card-body">
-                        <h4 class="card-title">Select node</h4>
-                        <p class="card-description"> The nodes are auto fetched from the panel, choose the one that you
+                        <h4 class="card-title">Select nest</h4>
+                        <p class="card-description"> The nests are auto fetched from the panel, choose the one that you
                             want to add.
                         </p>
-                        <form class="form-group" method="post" action={{ route('dashboard.nodes.add') }}>
+                        <form class="form-group" method="post" action={{ route('dashboard.nests.add') }}>
                             @csrf
                             <label for="node_id">Select target</label>
                             <select class="form-control form-control-lg mb-3" id="node_id" name="node_id">
-                                @foreach ($nodes as $node)
-                                <option value={{ $node['attributes']['id'] }}>Location ID: {{
-                                    $node['attributes']['location_id'] }} | {{
-                                    $node['attributes']['name'] }} | Memory: {{ $node['attributes']['memory']}} | Disk:
-                                    {{ $node['attributes']['disk']}}</option>
+                                @foreach ($nests as $nest)
+                                <option value={{ $nest['attributes']['id'] }}>Nest ID: {{
+                                    $nest['attributes']['id'] }} | {{
+                                    $nest['attributes']['name'] }}</option>
                                 @endforeach
                             </select>
                             @error('node_id')
                             <p>{{ $message }}</p>
                             @enderror
-
-                            <div class="form-group">
-                                <label for="node_name">Name</label>
-                                <input type="text" class="form-control" id="node_name" name="node_name"
-                                    placeholder="Name of the node (Displayed when deploying a server)" value={{
-                                    old('node_name') }}>
-                            </div>
-                            @error('node_name')
-                            <p>{{ $message }}</p>
-                            @enderror
-
-                            <div class="form-group">
-                                <label for="node_description">Description</label>
-                                <input type="text" class="form-control" id="node_description" name="node_description"
-                                    placeholder="Processor name perhaps?" value={{ old('node_description') }}>
-                            </div>
-                            @error('node_description')
-                            <p>{{ $message }}</p>
-                            @enderror
-
-                            <div class="form-group">
-                                <label for="node_slots">Slots</label>
-                                <input type="number" class="form-control" id="node_slots" name="node_slots"
-                                    placeholder="How many servers can be created on this node?" min="0" value={{
-                                    old('node_slots') }}>
-                            </div>
-                            @error('node_slots')
-                            <p>{{ $message }}</p>
-                            @enderror
-
-                            <button type="submit" class="btn btn-success mr-2">+ Add node</button>
+                            <button type="submit" class="btn btn-success mr-2">+ Add nest</button>
                         </form>
                     </div>
                 </div>
