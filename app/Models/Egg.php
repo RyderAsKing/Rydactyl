d68d74705a3e416f75a4122cbdcc4c63edf44157
RyderAsking <clasherlodt@gmail.com>
2021-11-18 20:03:38 +0530

    Relation between nests and eggs

--- app/Models/Egg.php
+++ app/Models/Egg.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Nest;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Egg extends Model
 {
     use HasFactory;
+
+    public function nest()
+    {
+        return $this->belongsTo(Nest::class);
+    }
 }
--- app/Models/Nest.php
+++ app/Models/Nest.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Egg;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Nest extends Model
 {
     use HasFactory;
+
+    public function egg()
+    {
+        return $this->hasMany(Egg::class);
+    }
 }
