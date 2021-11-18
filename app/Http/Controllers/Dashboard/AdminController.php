6d1eb963ffdfe7ca211a56043bc7b728a8a44e2b
RyderAsking <clasherlodt@gmail.com>
2021-11-18 20:04:10 +0530

    Bug fix, renamed nests() to get_nests()

--- app/Http/Controllers/Dashboard/AdminController.php
+++ app/Http/Controllers/Dashboard/AdminController.php
@@ -61,7 +61,7 @@ class AdminController extends Controller
 
     public function nest_add()
     {
-        $nests = Pterodactyl::nests()['data'];
+        $nests = Pterodactyl::get_nests()['data'];
         return view('dashboard.nests.add', ['nests' => $nests]);
     }
 
