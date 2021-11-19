ffd9f69557b470d0a152db7328478b878109c2dc
RyderAsking <clasherlodt@gmail.com>
2021-11-19 20:23:55 +0530

    Optimizations

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -3,12 +3,11 @@
 <script src={{ asset('vendors/js/vendor.bundle.base.js') }}></script>
 <!-- endinject -->
 <!-- Plugin js for this page -->
-<script src={{ asset('vendors/chart.js/Chart.min.js') }}></script>
-<script src={{ asset('vendors/owl-carousel-2/owl.carousel.min.js') }}></script>
+{{-- <script src={{ asset('vendors/chart.js/Chart.min.js') }}></script> --}}
 <script src={{ asset('js/datatables.min.js') }}></script>
 <!-- End plugin js for this page -->
 <!-- inject:js -->
-{{-- <script src={{ asset('js/off-canvas.js') }}></script> --}}
+<script src={{ asset('js/off-canvas.js') }}></script>
 <script src={{ asset('js/hoverable-collapse.js') }}></script>
 <script src={{ asset('js/settings.js') }}></script>
 <script src={{ asset('js/misc.js') }}></script>
--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -9,19 +9,12 @@
     <link rel="stylesheet" href={{ asset('vendors/mdi/css/materialdesignicons.min.css') }}>
     <link rel="stylesheet" href={{ asset('vendors/css/vendor.bundle.base.css') }}>
     <!-- endinject -->
-    <!-- Plugin css for this page -->
-    <link rel="stylesheet" href={{ asset('vendors/jvectormap/jquery-jvectormap.css') }}>
     <link rel="stylesheet" href={{ asset('vendors/flag-icon-css/css/flag-icon.min.css') }}>
-    <link rel="stylesheet" href={{ asset('vendors/owl-carousel-2/owl.carousel.min.css') }}>
-    <link rel="stylesheet" href={{ asset('vendors/owl-carousel-2/owl.theme.default.min.css') }}>
     <link rel="stylesheet" href="{{ asset('css/datatables.min.css') }}">
     <!-- End plugin css for this page -->
-    <!-- inject:css -->
-    <!-- endinject -->
     <!-- Layout styles -->
     <link rel="stylesheet" href={{ asset('css/style.css') }}>
     <!-- End layout styles -->
     <link rel="shortcut icon" href={{ asset('images/favicon.png') }} />
-    <script src={{ asset('js/jquery.js') }}></script>
 </head>
 @endsection
